c90a30af1ffdca77f098768ffcab3f0dbbc23ac0
mariorzgz <mario.rod.gonz97@gmail.com>
2022-04-13 13:04:35 +0200

    Added Projects content

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const projects = [
+  {
+    title: "Random Quote Machine",
+    subtitle: "React",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-1.gif",
+    link: "https://mariorzgz.github.io/random-quote-machine/",
+    year: "2022"
+  },
+  {
+    title: "Refresh",
+    subtitle: "Ruby on Rails",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-2.gif",
+    link: "https://www.refresh-your-style.com/",
+    year: "2022"
+  },
+  {
+    title: "Cuchillo Amigo",
+    subtitle: "Wordpress",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    image: "./project-3.gif",
+    link: "https://cuchilloamigo.com/",
+    year: "2019"
+  },
+];
