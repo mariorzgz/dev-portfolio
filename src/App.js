616b8527416cf1a524dd53427aa01c0a91ccd819
mariorzgz <mario.rod.gonz97@gmail.com>
2022-04-13 12:44:50 +0200

    Imported components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
-import logo from './logo.svg';
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Testimonials from "./components/Testimonials";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-
-      <h1 className="text-3xl font-bold underline">Hello World</h1>
-
-    </div>
+    <main className="text-gray-400 bg-gray-900 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
   );
 }
 
